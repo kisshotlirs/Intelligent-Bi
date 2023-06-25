71083dd5bf768ca03003b8744ad20ea55ce06884
mojo <3523915495@qq.com>
2023-06-25 22:56:12 +0800

    添加限流操作

--- src/main/java/com/mojo/bi/common/ErrorCode.java
+++ src/main/java/com/mojo/bi/common/ErrorCode.java
@@ -15,7 +15,9 @@ public enum ErrorCode {
     NOT_FOUND_ERROR(40400, "请求数据不存在"),
     FORBIDDEN_ERROR(40300, "禁止访问"),
     SYSTEM_ERROR(50000, "系统内部异常"),
-    OPERATION_ERROR(50001, "操作失败");
+    OPERATION_ERROR(50001, "操作失败"),
+
+    TOO_MANY_REQUEST(42900, "请求过于频繁");
 
     /**
      * 状态码
---
+++ src/main/java/com/mojo/bi/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.mojo.bi.config;
+
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author mojo
+ * @description: redis限流配置类
+ * @date 2023/6/25 0025 21:48
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisConfig {
+
+    private Integer database;
+
+    private String host;
+
+    private Integer port;
+
+    private String password;
+
+    @Bean
+    public RedissonClient getRedissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setDatabase(database)
+                .setAddress("redis://" + host + ":" + port)
+                .setPassword(password);
+        return Redisson.create(config);
+    }
+}
